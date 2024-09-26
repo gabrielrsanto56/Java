f6fddc6c89853331999b042594b907d58b166da6
WandrewRicardo <wandrewricardo@gmail.com>
2024-09-25 21:21:13 -0300

    Add files via upload

---
+++ PrimeiroPrograma.java
@@ -0,0 +1,11 @@
+
+package com.mycompany.primeiroprograma;
+
+
+public class PrimeiroPrograma {
+    public static void main(String[]args){
+    System.out.print("ola Mundo!\n"); //esta sendo usado comando para impressão na tela
+    
+    }
+    
+}
